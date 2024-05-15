e556281813902fec3f21125fc9977ce93eebfb0e
Yashitjain <72515029+Yashitjain@users.noreply.github.com>
2024-05-15 12:51:45 +0530

    Update index.js
    player can roll dice only alternatively

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-
+var yellow_chance=true;
+var blue_Chance=false;
 var yellowcoin = 1;
-document.querySelector(".dice1").addEventListener("click",function(){
-    document.getElementById("block"+yellowcoin).classList.remove("yellowcoin_block");
-    document.getElementById("blockimg"+yellowcoin).classList.remove("current_yellow_block_img");
-    random = Math.floor(Math.random(0,1)*6)+1;
-    document.querySelector(".dice_img1").setAttribute("src","./dice images/dice"+random+".png");
-    if(100>=yellowcoin+random){
-        yellowcoin=yellowcoin+random;
+document.querySelector(".dice_img1").addEventListener("click",function(){
+    if(yellow_chance==true){
+        document.getElementById("block"+yellowcoin).classList.remove("yellowcoin_block");
+        document.getElementById("blockimg"+yellowcoin).classList.remove("current_yellow_block_img");
+        random = Math.floor(Math.random(0,1)*6)+1;
+        document.querySelector(".dice_img1").setAttribute("src","./dice images/dice"+random+".png");
+        if(100>=yellowcoin+random){
+            yellowcoin=yellowcoin+random;
+        }
+        yellowcoin=stair_snake(yellowcoin);
+        console.log("yellowcon=>"+yellowcoin);
+        setTimeout(() => {
+            document.getElementById("block"+yellowcoin).classList.add("yellow_block");
+            document.getElementById("blockimg"+yellowcoin).classList.add("current_yellow_block_img");
+        }, 500);
     }
-    yellowcoin=stair_snake(yellowcoin);
-    console.log("yellowcon=>"+yellowcoin);
-    setTimeout(() => {
-        document.getElementById("block"+yellowcoin).classList.add("yellow_block");
-        document.getElementById("blockimg"+yellowcoin).classList.add("current_yellow_block_img");
-    }, 1000);
+    document.getElementById("dice_img1").classList.remove("dice_img_chance");
     winner(yellowcoin,"YELLOW");
+    yellow_chance=false;
+    blue_Chance=true;
     
 })
 
 
 var bluecoin = 1;
-document.querySelector(".dice2").addEventListener("click",function(){
-    document.getElementById("block"+bluecoin).classList.remove("blue_block");
-    document.getElementById("blockimg"+bluecoin).classList.remove("current_blue_block_img");
-    random = Math.floor(Math.random(0,1)*6)+1;
-    document.querySelector(".dice_img2").setAttribute("src","./dice images/dice"+random+".png");
-    if(100>=bluecoin+random){
-        bluecoin=bluecoin+random;
+document.querySelector(".dice_img2").addEventListener("click",function(){
+    if(blue_Chance==true){
+        document.querySelector("#dice_img2").classList.add("dice_img_chance");
+        document.getElementById("block"+bluecoin).classList.remove("blue_block");
+        document.getElementById("blockimg"+bluecoin).classList.remove("current_blue_block_img");
+        random = Math.floor(Math.random(0,1)*6)+1;
+        document.querySelector(".dice_img2").setAttribute("src","./dice images/dice"+random+".png");
+        if(100>=bluecoin+random){
+            bluecoin=bluecoin+random;
+        }
+        bluecoin=stair_snake(bluecoin);
+        console.log("yellowcoin=>"+yellowcoin);
+        setTimeout(() => {
+            document.getElementById("block"+bluecoin).classList.add("blue_block");
+            document.getElementById("blockimg"+bluecoin).classList.add("current_blue_block_img");
+        }, 500);
+
     }
-    bluecoin=stair_snake(bluecoin);
-    console.log("yellowcoin=>"+yellowcoin);
-    setTimeout(() => {
-        document.getElementById("block"+bluecoin).classList.add("blue_block");
-        document.getElementById("blockimg"+bluecoin).classList.add("current_blue_block_img");
-    }, 1000);
-    
+    document.getElementById("dice_img2").classList.remove("dice_img_chance");
     winner(bluecoin ,"BLUE");
+    blue_Chance=false;
+    yellow_chance=true;
     
 })
 
@@ -80,8 +92,10 @@ function stair_snake(random){
 
 function winner(random,colour){
     if(random==100){
-        document.getElementById("heading").textContent=colour+" WINS";
+        document.getElementById("heading").textContent=colour+"   WINS";
         console.log(document.getElementById("heading").innerHTML);
+        yellow_chance=false;
+        blue_Chance=false;
     }
 
 }
